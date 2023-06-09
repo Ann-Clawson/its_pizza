fea214d30b1fd1e0b38b7efd9b6f48b587ecebb2
Mrs. Clawson <112268033+theannager@users.noreply.github.com>
2023-06-09 11:20:43 -0700

    Initalized repository

---
+++ main.js
@@ -0,0 +1,24 @@
+const toppingChoices = ['Pepperoni', 'Broccoli', 'Extra Cheese', 'Sand', 'Flintstones Vitamins', 'Sausage', 'Banana Peppers', 'Grapes', 'Black Olives', 'Onions', 'Green Bell Peppers', 'Mushrooms', 'Mushrooms (wink wink)', 'Rhubarb', 'Canadian Bacon', 'Garlic', 'Yogurt', 'Oregano']
+
+function getToppings() {
+    let toppings = [];
+    let i=0;
+    do{
+        toppings.push(toppingChoices[Math.floor(Math.random()*17)]);
+        i++;
+    }while (i<5);
+    return toppings;
+};
+
+let randoResults = document.getElementById('rando-results');
+
+let button = document.getElementById('button');
+
+/*function turnBlue(){
+    button.style.backgroundColor='blue'
+};*/
+
+
+button.onclick = function() {
+    randoResults.innerHTML = getToppings()
+};
