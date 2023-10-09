3b5a8d8dedd351ba33c1464d54b63db39deaa5d8
Ann Clawson <annmasonclawson@gmail.com>
2023-10-09 15:40:07 -0700

    Fix punctuation

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const toppingChoices = ['Pepperoni', 'Broccoli', 'Extra Cheese', 'Sand', 'Flintstones Vitamins', 'Sausage', 'Banana Peppers', 'Grapes', 'Black Olives', 'Onions', 'Green Bell Peppers', 'Mushrooms', 'Mushrooms (wink wink)', 'Rhubarb', 'Canadian Bacon', 'Garlic', 'Yogurt', 'Oregano']
+const toppingChoices = [' Pepperoni', ' Broccoli', ' Extra Cheese', ' Sand', ' Flintstones Vitamins', ' Sausage', ' Banana Peppers', ' Grapes', ' Black Olives', ' Onions', ' Green Bell Peppers', ' Mushrooms', ' Rhubarb', ' Canadian Bacon', ' Garlic', ' Yogurt', ' Oregano']
 
 function getToppings() {
     let toppings = [];
     let i=0;
     do{
-        toppings.push(toppingChoices[Math.floor(Math.random()*17)]);
+        toppings.push(toppingChoices[Math.floor(Math.random()*toppingChoices.length)]);
         i++;
     }while (i<5);
     return toppings;
